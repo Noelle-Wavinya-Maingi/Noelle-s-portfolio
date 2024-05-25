8fd0d96968f15b4792fc4cbf33cd6acd73dc204e
Noelle-Wavinya-Maingi <noemaingi@gmail.com>
2024-05-25 19:33:48 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,102 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*=============== MENU SHOW ===============*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*=============== MENU HIDDEN ===============*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  //When clicking on each nav__link, we remove the show-menu
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== SWIPER PROJECTS ===============*/
+let swiperProjects = new Swiper(".projects__container", {
+  loop: true,
+  spaceBetween: 24,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: -56,
+    },
+  },
+});
+
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+  contactName = document.getElementById('contact-name'),
+  contactEmail = document.getElementById('contact-email'),
+  contactProject =  document.getElementById('contact-project')
+  contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+  e.preventDefault()
+
+  //Check if the field has a value
+  if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+    //Add and remove color
+    contactMessage.classList.remove('color-blue')
+    contactMessage.classList.add('color-red')
+
+    //Show message
+    contactMessage.textContent = 'Write all the input fields!'
+  }else {
+    //serviceID - templateID - #form -publicKey
+    emailjs.sendForm('service_9pl818r', 'template_lldh1sn', '#contact-form', 'nUWjG4TMe4uHMn2Mg')
+    .then(() => {
+      //Show message and add color
+      contactMessage.classList.add('color-blue')
+      contactMessage.textContent = 'Message sent ☑️'
+
+      //Remove message after 5 seconds
+      setTimeout(() => {
+        contactMessage.textContent = ''
+      }, 5000)
+    }, (error) => {
+      alert('OOPS!, Something has failed...', error)
+    })
+
+    //To clear the input field
+    contactEmail.value = ''
+    contactName.value = ''
+    contactProject.value = ''
+  }
+}
+contactForm.addEventListener('submit', sendEmail)
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== DARK LIGHT THEME ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
